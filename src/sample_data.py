d06fa1701bb9679cbebcde71f3e9b63e852b0d62
pradeepverma884027-cell <pradeepverma884027@gmail.com>
2026-06-22 22:39:59 +0530

    Create sample_data.py

---
+++ src/sample_data.py
@@ -0,0 +1,48 @@
+# sample_data.py
+
+sample_text_1 = """
+Customer Support Ticket
+
+Name: Pradeep Kumar
+Order Number: ORD78945
+Email: pradeepkumar@gmail.com
+Phone: 9876543210
+
+Hello Team,
+
+I recently placed an order through your website.
+Please provide the shipping status.
+
+Thank you,
+Pradeep Kumar
+"""
+
+sample_text_2 = """
+Hi,
+
+This is Rahul Singh.
+
+My email is rahul@gmail.com.
+
+I need help with my account.
+
+Regards,
+Rahul Singh
+"""
+
+sample_text_3 = """
+Customer Complaint
+
+Phone: 9123456780
+Order Number: ORD45678
+
+The delivered item is damaged.
+
+Please arrange a replacement.
+"""
+
+sample_data = [
+    sample_text_1,
+    sample_text_2,
+    sample_text_3
+]
